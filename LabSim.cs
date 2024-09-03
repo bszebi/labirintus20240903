e9b3ed3802ef4b65b10a77d94c150a58296edb8f
bszebi <120450969+bszebi@users.noreply.github.com>
2024-09-03 11:20:48 +0200

    Add project files.

---
+++ LabSim.cs
@@ -0,0 +1,19 @@
+﻿namespace labirintus0903
+{
+    internal class LabSim
+    {
+        private List<string> adatsorok;
+        private char[,] lab;
+
+        public void AdatsorokBeolvasasa(string forras)
+        {
+            using StreamReader sr = new(forras);
+            while (!sr.EndOfStream) adatsorok.Add(sr.ReadLine());
+        }
+        public LabSim(string fileName)
+        {
+            AdatsorokBeolvasasa(@$"..\..\..\src\{fileName}");
+
+        }
+    }
+}
